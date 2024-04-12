798cf0f5bbdaa0cf3cdb4f55e69cd9999ce53e62
Sandakalum <hgdsandakalum@gmail.com>
2024-04-12 02:17:28 +0200

    install shadecn

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,12 @@
 import { Metadata } from 'next';
 import "@/styles/global.css";
+import { Inter as FontSans } from "next/font/google"
+import { cn } from "@/lib/utils"
+
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+})
  
 export const metadata = {
     applicationName: 'Early Sight',
@@ -13,7 +20,12 @@ function RootLayout({ children }) {
     
     return (
       <html lang="en" className="h-full bg-white">
-        <body className="h-full">{children}</body>
+        <body  className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          fontSans.variable
+        )}>
+          {children}
+        </body>
       </html>
     );
   }
