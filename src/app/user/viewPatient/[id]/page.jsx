edf7e74974376d0207f2f41d32a018e096646216
FazidSamoon <tuan.fazid@outlook.com>
2024-10-23 21:49:27 +0530

    route issue fixed

--- src/app/user/viewPatient/[id]/page.jsx
+++ src/app/user/viewPatient/[id]/page.jsx
@@ -1,21 +1,35 @@
-"use client";
-import { useRouter } from "next/router";
+
 import React from "react";
 import PatientOverview from "@/components/PatientOverview/PatientOverview";
 
-const page = () => {
-  // const router = useRouter();
-  // const { id } = router.query;
-  // console.log(id)
-  // const {  } = router.query;
+export async function generateStaticParams() {
+  try {
+    const response = await fetch("http://192.168.8.138:3005/api/v1/patients");
+
+    if (!response.ok) {
+      throw new Error("Failed to fetch patient IDs");
+    }
+
+    const data = await response.json();
+
+    const patientIds = data.map((patient) => ({
+      id: patient._id.toString(),
+    }));
+    console.log("patientIds ", patientIds);
+    return patientIds
+  } catch (error) {
+    console.error("Error fetching patient data:", error);
+    return [];
+  }
+}
+
+const PatientViewPage = () => {
   return (
-    <>
-      <div className="container mx-auto">
-        <span className=" text-lg font-bold">Patients Overview</span>
-        <PatientOverview />
-      </div>
-    </>
+    <div className="container mx-auto py-6">
+   
+      <PatientOverview />
+    </div>
   );
 };
 
-export default page;
+export default PatientViewPage;
